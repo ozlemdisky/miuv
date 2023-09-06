37a768faa556246ad5ab78152bd2691b6aff1be8
Özlem Dişkaya <81317620+ozlemdisky@users.noreply.github.com>
2023-09-06 17:05:50 +0300

    commit

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,31 +1,61 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Grass from "../constans/design/grass";
+import Grassmini from "../constans/design/grassmini";
+import GrassminiTwo from "../constans/design/grassminiTwo";
+import { Image } from "react-native";
 import { StyleSheet } from "react-native";
 
 const HomePage = () => {
   return (
     <View style={styles.container}>
-    <Text style={{ color: "blue" }}>hellööö basliyoruzzz</Text>
-    <View style={styles.grassContainer}>
-      <Grass />
+      <Text style={{ color: "blue" }}>hellööö basliyoruzzz</Text>
+      <View style={styles.grassContainer }>
+        <Grass />
+      </View>
+      <View style={styles.grassMini}>
+      <Grassmini />
+      </View>
+      <View style={styles.grassMiniContainer}>
+        <GrassminiTwo/>
+      </View>
+      <View style={styles.image}>
+      <Image source={require('../images/lionn.png')} />
+      </View>
     </View>
-  </View>
-);
+  );
 };
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  alignItems: "center",
-  justifyContent: "center",
-  
-},
-grassContainer: {
-  position: "absolute",
-  bottom: 0, 
-  
-},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  grassContainer: {
+    position: "absolute",
+    bottom: 0,
+    zIndex: 2,
+  },
+  grassMini: {
+    position: "absolute",
+    bottom: 287,
+    left: 120,
+  },
+  grassMiniContainer: {
+    position: "absolute",
+    right: 116 ,
+    bottom: 260,
+  },
+  image:{
+    zIndex:1,
+    bottom: 35,
+    height:400,
+    width: 400,
+
+
+
+  }
 });
 
 export default HomePage;
---
+++ constans/design/grassminiTwo.js
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Svg, Path } from "react-native-svg";
+
+const GrassminiTwo = () => {
+  return (
+    <Svg
+      width="156"
+      height="118"
+      viewBox="0 0 149 110"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <Path
+        d="M-5 20.913C4.14264 17.8487 23.685 18.768 28.7135 46.9598C34.6181 37.9371 49.6273 25.3052 62.427 46.9598C41.2846 -5.64457 84.7122 -15.859 84.7122 25.5096C84.7122 15.321 103.668 -3.39131 123.192 20.2816C124.513 17.1625 131.479 17.6167 142.5 25.5096C122.951 36.3 130.997 62.7922 130.997 79.6461C110.158 79.6461 50.9987 52.067 79.5694 113.354C63.7603 104.161 24.7136 92.312 -5 118.461V20.913Z"
+        fill="#48AC0B"
+        fill-opacity="0.86"
+      />
+    </Svg>
+  );
+};
+
+export default GrassminiTwo;
